51e1a332f8ba5bde82c083043fc5cd3723b088d2
KaykyFidelis <kaykyfidelisserafim@gmail.com>
2023-09-15 17:52:20 -0300

    Roteiro de Revisão 01 versão final

--- RR1-01/src/main/java/banco/AuditorGenerico.java
+++ RR1-01/src/main/java/banco/AuditorGenerico.java
@@ -4,27 +4,28 @@ public class AuditorGenerico {
 
   private static final double LIMITE = 500;
 
-  boolean investigaBanco(QualquerBanco b){
+  boolean investigaBanco(QualquerBanco b) {
     double sm;
-    sm = b.saldoTotal()/b.numContas(); 
+    sm = b.saldoTotal() / b.numContas();
     return (sm > LIMITE);
   }
 
   public static void main(String[] args) {
-  //instanciar um auditor generico
-	  AuditorGenerico a = new AuditorGenerico();
-	  
-  //instanciar um banco de seguros
-	  QualquerBanco bs = new BancoSeguros();
-	  
-  //instanciar um banco de investimentos
-	  QualquerBanco bi = new BancoInvestimentos();
+    // instanciar um auditor generico
 
-  //mandar o auditor investigar o banco de seguros
-	  a.investigaBanco(bs);
-	  
-  //mandar o auditor investigar o banco de investimentos
-	  a.investigaBanco(bi);
-	  
+    AuditorGenerico a = new AuditorGenerico();
+
+    // instanciar um banco de seguros
+    QualquerBanco bs = new BancoSeguros();
+
+    // instanciar um banco de investimentos
+    QualquerBanco bi = new BancoInvestimentos();
+
+    // mandar o auditor investigar o banco de seguros
+    a.investigaBanco(bs);
+
+    // mandar o auditor investigar o banco de investimentos
+    a.investigaBanco(bi);
+
   }
 }
